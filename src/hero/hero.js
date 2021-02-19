101f13fb635ed335e1cd05fc89653238bcc01b8b
asrafulnirob1376 <asrafulsharkernirob1376@gmail.com>
2021-02-20 03:23:03 +0600

    add font

--- src/hero/hero.js
+++ src/hero/hero.js
@@ -6,11 +6,11 @@ function hero() {
             <div className="container" style={{paddingTop:"50px"}} >
                 <div className=" m-auto">
                     <img style={{width:"100%"}} src={Hero1}/>
-                    <h1 style={{textAlign:"center" ,fontSize:"50px",padding:"20px 10px 20px 10px"}}>A few words about this blog platform, Ghost, and how this site was made</h1>
+                    <h1 style={{textAlign:"center" ,fontSize:"50px",padding:"20px 10px 20px 10px",fontFamily:"Abril Fatface"}}>A few words about this blog platform, Ghost, and how this site was made</h1>
                     
                 </div>
                 <div className="col-md-10 m-auto">
-                <p className="m-auto" style={{textAlign:"center", borderBottom:"2px solid black"}}>Why ghost (& Figma) instead of mediam, wordpress or other platform?</p>
+                <p className="m-auto" style={{textAlign:"center", borderBottom:"2px solid black",fontFamily:"Slabo 27px"}}>Why ghost (& Figma) instead of mediam, wordpress or other platform?</p>
                 </div>
             </div>
         </div>
